742001f558f6e7d7065d16727e6972ab34e97a9f
ibat10clw <tyjkhskjs957@gmail.com>
2025-03-02 22:28:24 +0800

    Implement q_insert_head and q_insert_tail
    Allocate memory for a new node and duplicate the input string
    into its buffer. If memory allocation or string duplication
    fails, properly free allocated memory to prevent leaks.
    
    Use list_add for head insertion and list_add_tail for tail
    insertion to maintain correct queue structure.
    
    Change-Id: I50047715e1065f5e1b6c4df475e78d6e44beddfc

--- queue.c
+++ queue.c
@@ -20,12 +20,40 @@ void q_free(struct list_head *head) {}
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    if (!head || !s)
+        return false;
+
+    element_t *node = (element_t *) malloc(sizeof(element_t));
+    if (!node)
+        return false;
+
+    node->value = strdup(s);
+    if (!node->value) {
+        free(node);
+        return false;
+    }
+
+    list_add(&node->list, head);
     return true;
 }
 
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    if (!head || !s)
+        return false;
+
+    element_t *node = (element_t *) malloc(sizeof(element_t));
+    if (!node)
+        return false;
+
+    node->value = strdup(s);
+    if (!node->value) {
+        free(node);
+        return false;
+    }
+
+    list_add_tail(&node->list, head);
     return true;
 }
 
