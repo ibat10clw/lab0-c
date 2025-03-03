f70b3d8979c79bb17beaa85a606f81e97c952919
ibat10clw <tyjkhskjs957@gmail.com>
2025-03-03 14:48:40 +0800

    Implement q_free
    Traverse the queue and free each node along with its stored
    string to prevent memory leaks.
    
    Change-Id: I2ec9793de363dcd8b9da90f7731785e58fa2fc89

--- queue.c
+++ queue.c
@@ -15,7 +15,18 @@ struct list_head *q_new()
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (!head)
+        return;
+
+    element_t *it, *tmp = NULL;
+    list_for_each_entry_safe (it, tmp, head, list) {
+        free(it->value);
+        free(it);
+    }
+    free(head);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
