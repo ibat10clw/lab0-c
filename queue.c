6d8b800a50ac403412463585a0bde53c62b294c5
ibat10clw <tyjkhskjs957@gmail.com>
2025-03-05 00:14:10 +0800

    Implement q_delete_mid
    Use two pointer approach to find the middle element.
    
    Change-Id: Ic31cc48c6d05b263a34bcb2285f928db25b712c8

--- queue.c
+++ queue.c
@@ -123,6 +123,19 @@ int q_size(struct list_head *head)
 bool q_delete_mid(struct list_head *head)
 {
     // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+    if (!head || list_empty(head))
+        return false;
+
+    struct list_head *slow = head->next, *fast = head->next->next;
+
+    while (fast != head && fast->next != head) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    element_t *tmp = list_entry(slow, element_t, list);
+    list_del(slow);
+    free(tmp->value);
+    free(tmp);
     return true;
 }
 
